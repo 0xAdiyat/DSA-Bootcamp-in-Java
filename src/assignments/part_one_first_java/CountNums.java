6878a1c5c75e3e9abeeb3349766e750011b02368
Huss4in007 <smdhussainmolla@gmail.com>
2023-06-09 21:31:06 +0600

    Nums & Strings Palindrome

---
+++ src/assignments/part_one_first_java/CountNums.java
@@ -0,0 +1,18 @@
+package assignments.part_one_first_java;
+
+public class CountNums {
+    public static void main(String[] args) {
+        int n = 2332, target = 3;
+        int count = 0;
+        while (n > 0) {
+            int reminder = n % 10;
+            if (reminder == target){
+                count++;
+        }
+            n = n / 10;
+    }
+
+        System.out.println(count);
+
+    }
+}
