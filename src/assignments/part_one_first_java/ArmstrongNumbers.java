c1366498b1a935d3e3195e34b11c2f00d7db8edb
Huss4in007 <smdhussainmolla@gmail.com>
2023-06-10 08:51:25 +0600

    ArmstrongNumbers

---
+++ src/assignments/part_one_first_java/ArmstrongNumbers.java
@@ -0,0 +1,37 @@
+package assignments.part_one_first_java;
+
+public class ArmstrongNumbers {
+    public static void main(String[] args) {
+
+        int lowerBound = 100, upperBound = 500;
+
+        for (int number = lowerBound; number <= upperBound; number++) {
+            if (isArmstrong(number)) {
+                System.out.println(number);
+            }
+
+
+        }
+
+    }
+
+    static boolean isArmstrong(int num) {
+
+        int originalNumber = num;
+        int sum = 0;
+
+        while (num > 0) {
+
+            int reminder = num % 10;
+            num /= 10;
+            int cubicReminder = reminder * reminder * reminder;
+            sum += cubicReminder;
+
+        }
+
+
+        return sum == originalNumber;
+    }
+
+
+}
